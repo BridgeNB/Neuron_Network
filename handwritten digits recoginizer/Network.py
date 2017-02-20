43a78253445029a56c8d6b64858eecb07cb2019e
BridgeNB <zyq123258789@gmail.com>
2017-02-20 14:49:50 -0500

    test handwritten digits

--- handwritten digits recoginizer/Network.py
+++ handwritten digits recoginizer/Network.py
@@ -63,5 +63,60 @@ class Network(object):
         self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, init_b)]
         self.biases = [b - (eta/ len(mini_batch)) * nb for b, nb in zip(self.biases, init_w)]
 
+    def backprop(self, x, y):
+        """
+        :param x:
+        :param y:
+        :return: a tuple of (x, y) representing the gradient for the cost function C_x
+        """
+        init_b = [np.zeros(b.shape) for b in self.biases]
+        init_w = [np.zeros(w.shape) for w in self.weights]
+
+        # forward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+
+        init_b[-1] = delta
+        init_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            init_b[-l] = delta
+            init_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+
+        return (init_b, init_w)
+
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations-y)
+
+
+
+
 def sigmoid(self, z):
     return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
